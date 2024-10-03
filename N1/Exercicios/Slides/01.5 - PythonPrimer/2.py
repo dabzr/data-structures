f9c7f96fe60f62d884ff2d26f9638166fc803fa5
dabzr <dylanab@pm.me>
2024-10-03 13:04:47 -0300

    Melhorando o código das questões 2 e 3

--- N1/Exercicios/Slides/01.5 - PythonPrimer/2.py
+++ N1/Exercicios/Slides/01.5 - PythonPrimer/2.py
@@ -3,11 +3,8 @@ Escreva uma função em Python que receba um inteiro positivo n
 e retorne a soma dos quadrados de todos os inteiros positivos menores do que n.
 """
 
-from functools import reduce
-from operator import add
-
 def square_sum(n: int) -> int:
-    return reduce(add, map(lambda x: x*x, range(n)))
+    return sum(map(lambda x: x*x, range(n)))
 
 if __name__ == "__main__":
     print(square_sum(5))
--- N1/Exercicios/Slides/01.5 - PythonPrimer/3.py
+++ N1/Exercicios/Slides/01.5 - PythonPrimer/3.py
@@ -3,11 +3,8 @@ Escreva uma função em Python que receba um inteiro positivo n
 e retorne a soma dos quadrados de todos os inteiros positivos pares menores do que n.
 """
 
-from functools import reduce
-from operator import add
-
 def square_sum_even(n: int) -> int:
-    return reduce(add, filter(lambda x: x%2 == 0, map(lambda x: x*x, range(n))))
+    return sum(filter(lambda x: x%2 == 0, map(lambda x: x*x, range(n))))
 
 if __name__ == "__main__":
     print(square_sum_even(5))
