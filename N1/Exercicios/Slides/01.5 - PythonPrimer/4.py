078a8964bd323d416f87532d617c5bec3dabd261
dabzr <dylanab@pm.me>
2024-10-06 23:15:44 -0300

    removendo import desnecessário

--- N1/Exercicios/Slides/01.5 - PythonPrimer/4.py
+++ N1/Exercicios/Slides/01.5 - PythonPrimer/4.py
@@ -3,9 +3,7 @@ Escreva uma função em Python que receba uma sequência de números
 e determine se todos os números são diferentes um dos outros.
 """
 
-from typing import List
-
-def have_repeated_items(lista: List[int]) -> bool:
+def have_repeated_items(lista: list) -> bool:
     return len(set(lista)) != len(lista)
 
 if __name__ == "__main__":
