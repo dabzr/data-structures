47ac1c678eccc9c83f96c471c792972ceb9b88b4
dabzr <128552152+dabzr@users.noreply.github.com>
2025-02-03 11:39:18 -0300

    Create bloom.py

---
+++ bloom.py
@@ -0,0 +1,48 @@
+import mmh3
+from faker import Faker
+fake = Faker()
+from random import sample
+
+class BitArray:
+    def __init__(self, size):
+        self.barray = [0] * (size%32)
+    def set(self, idx):
+        i = idx%(len(self.barray))
+        bi = idx%32
+        self.barray[i] |= 1 << bi 
+    def get(self, idx):
+        i = idx%(len(self.barray))
+        bi = idx%32
+        return (self.barray[i] >> bi) & 1
+
+class Lista:
+    def __init__(self, barray_size, hash_quantity):
+        self.list = []
+        self.size = barray_size
+        self.bit_array = BitArray(barray_size)
+        self.seeds = sample(range(1, 141115), hash_quantity)
+    def append(self, item):
+        self.list.append(item)
+        for seed in self.seeds:
+            self.bit_array.set(mmh3.hash(str(item), signed=False, seed=seed)%self.size)
+    def __contains__(self, key):
+        for seed in self.seeds:
+            if self.bit_array.get(mmh3.hash(str(key), signed=False, seed=seed)%self.size) != 1:
+                return False
+        return True
+
+quantities = [50, 250, 500]
+hashes = [3, 6, 9]
+spans = [50, 180, 350]
+emails = [200, 500, 1000]
+for quantity in quantities:
+    for hashy in hashes:
+        for span in spans:
+            for email in emails:
+                l = Lista(quantity, hashy)
+                for i in range(span):
+                    l.append(fake.email())
+                randemails = [fake.email() for i in range(email)]
+                print(f"{quantity}, {hashy}, {span}, {emails}",end=": ")
+                print(len(list(filter(lambda x: x in l, randemails))))
+
